540bcf09360c42a9d9707d33d5b2dadb4ac285a5
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-11-12 14:24:28 +0530

    Extend tailwind using layers

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -55,7 +55,7 @@ const SearchParams = () => {
           <select
             id="animal"
             name="animal"
-            className="mb-5 block w-60"
+            className="search-input"
             onChange={(e) => {
               setAnimal(e.target.value);
             }}
@@ -75,7 +75,7 @@ const SearchParams = () => {
         <label htmlFor="breed">
           Breed
           <select
-            className="mb-5 block w-60 disabled:opacity-50"
+            className="search-input grayed-out-disabled"
             disabled={!breeds.length}
             id="breed"
             name="breed"
